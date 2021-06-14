4f2b2a975bc49eb0f125d9da8a2bc6ee53f99d11
AIPTU <85402087+AIPTU@users.noreply.github.com>
2021-06-14 22:24:37 +0700

    Instance

--- src/AndreasHGK/Emotes/session/SessionManager.php
+++ src/AndreasHGK/Emotes/session/SessionManager.php
@@ -16,7 +16,7 @@ class SessionManager {
      * @return $this
      */
     public static function getInstance() : self {
-        if(!isset(self::$instance)) self::$instance = new self;
+        if(!isset(self::$instance)) self::$instance = $this;
         return self::$instance;
     }
 
